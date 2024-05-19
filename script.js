8b5810bd5dd4714f46dc6a161da241ef6fe704e6
lopponming <miranda.klockars@gmail.com>
2024-05-19 18:37:18 +0300

    toggle grayscale WORKS !!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// underlined active page for nav bar
 document.addEventListener("DOMContentLoaded", function() {
     var path = window.location.pathname;
     var page = path.split("/").pop();
@@ -11,3 +11,25 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+// toggle grayscale in accessibility dropdown
+document.addEventListener("DOMContentLoaded", function() {
+    // check if grayscale mode is enabled in localStorage
+    const isGrayscale = localStorage.getItem('grayscale') === 'true';
+    
+    if (isGrayscale) {
+        document.body.classList.add('grayscale');
+    }
+
+    document.querySelector('.dropdown-content ul li:nth-child(4)').addEventListener('click', function() {
+        toggleGrayscale();
+    });
+
+    function toggleGrayscale() {
+        const body = document.body;
+        body.classList.toggle('grayscale');
+        localStorage.setItem('grayscale', body.classList.contains('grayscale'));
+    }
+});
+
